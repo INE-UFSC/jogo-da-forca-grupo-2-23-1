5973c3e15da2e39779e63caf68c21561c5d1a1ff
Thiago Kenzo Takahashi <thiagokenzo7@gmail.com>
2023-03-15 19:48:43 -0300

    lista de letras já usadas foi adicionado

--- corretor.py
+++ corretor.py
@@ -1,11 +1,14 @@
 letra_jogador = input()
+letra_jogada = ["w"]
 
 #x é a letra do jogador
 #y é a palavra
 def corretor(x, y):
     for letra_da_palavra in list(y):
-        if x==letra_da_palavra:
+        if x in letra_jogada:
+            return False
+        elif x==letra_da_palavra:
             return True
-            break
         else: 
             return False
+    letra_jogada.append(x)
